61cfd84dc34a0077a0a07e91591066aebc10af69
Georgina <georginaxpuig@gmail.com>
2024-10-03 23:14:28 -0300

    PreEntrega3-Puig

--- js/main.js
+++ js/main.js
@@ -1,58 +1,61 @@
 // Calcular pagos en cuotas sobre un monto determinado.
 
+const elMonto = document.getElementById('monto');
+const elCuotas = document.getElementById('cuotas');
+const elInputsContainer = document.getElementById('inputsContainer');
+const btn = document.getElementById('button');
+const elPago = document.getElementById('pago');
+const elError = document.getElementById('error');
+
 class CalculadoraCuotas {
-    constructor() {
-        this.monto = 0;
-        this.cuotas = 0;
-        this.pago = 0;
+  constructor() {
+    this.monto = 0;
+    this.cuotas = 0;
+    this.pago = 0;
+    this.arrPagos = [];
+  }
+
+  calcularCuotas() {
+    this.pago = (parseFloat(this.monto) / parseInt(this.cuotas)).toFixed(2);
+  }
+
+  pagos() {
+    this.arrPagos.push(this.pago);
+    const totalPagos = this.arrPagos.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    return totalPagos;
+  }
+
+  init() {
+    btn.addEventListener('click', (e) => {
+      this.monto = elMonto.value;
+      this.cuotas = elCuotas.value;
+
+      if (isNaN(this.monto) || this.monto <= 0) {
+        alertMontoValido();
+      } else if (isNaN(this.cuotas) || this.cuotas <= 0) {
+        alertCuotaValida();
+      } else {
+        this.calcularCuotas();
+
+        console.log(this.pago);
+        console.log(this.arrPagos);
+
+        elError.innerHTML = '';
+        elPago.innerHTML = `<p>El monto de cada cuota es: ${this.pago}</p>`;
         this.arrPagos = [];
-    }
-
-    calcularCuotas() {
-        this.pago = parseFloat(this.monto) / parseInt(this.cuotas);
-    }
-
-    pagos() {
-      this.arrPagos.push(this.pago);
-      const totalPagos = this.arrPagos.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
-      console.log(`total de pagos ${totalPagos}`)
-    }
-
-    init() {
-      while (this.monto !== 'exit') {
-        this.monto = prompt('Ingrese el monto del producto. Escriba exit para salir del programa');
-
-        if (this.monto === 'exit') { break; }
-
-        if (isNaN(this.monto) || this.monto <= 0) {
-          alertMontoValido();
-          continue;
-        }
-
-        while (this.cuotas !== 'exit') {
-          this.cuotas = prompt('Ingrese el numero de cuotas.');
-
-          if (this.cuotas === 'exit') { break; }
-
-          if (isNaN(this.cuotas) || this.cuotas <= 0) {
-            alertCuotaValida();
-            continue;
-          }
-
-          this.calcularCuotas();
-          alert('El monto de cada cuota es : ' + this.pago);
-          break;
-        }
-
-        this.pagos();
-        this.pago = 0;
       }
-
-    }
+    });
+  }
 }
 
-const alertMontoValido = () => { alert('Ingrese un monto valido del producto.'); }
-const alertCuotaValida = () => { alert('Ingrese el numero mayor a 0.'); }
-
+const alertMontoValido = () => {
+  elError.innerHTML = `<p>Ingrese un monto valido del producto.</p>`;
+};
+const alertCuotaValida = () => {
+  elError.innerHTML = `<p>IIngrese el numero mayor a 0.`;
+};
 const calculadora = new CalculadoraCuotas();
 calculadora.init();
